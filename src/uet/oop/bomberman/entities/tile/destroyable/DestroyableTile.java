32adfa9b51fc6f2e3854da9c443bf832fcd4bcad
huynh1234 <42998636+huynh1234@users.noreply.github.com>
2018-12-02 09:31:45 +0700

    update big work
    update big work

--- src/uet/oop/bomberman/entities/tile/destroyable/DestroyableTile.java
+++ src/uet/oop/bomberman/entities/tile/destroyable/DestroyableTile.java
@@ -1,6 +1,7 @@
 package uet.oop.bomberman.entities.tile.destroyable;
 
 import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.entities.bomb.Flame;
 import uet.oop.bomberman.entities.tile.Tile;
 import uet.oop.bomberman.graphics.Sprite;
 
@@ -36,8 +37,13 @@ public class DestroyableTile extends Tile {
 	
 	@Override
 	public boolean collide(Entity e) {
-		// TODO: xử lý khi va chạm với Flame
+		// TODO: xử lý khi va chạm vớie Flame
+		if(e instanceof Flame){
+			destroy();
+
+		}
 		return false;
+
 	}
 	
 	public void addBelowSprite(Sprite sprite) {
